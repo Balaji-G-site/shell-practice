d4f8769a9b3b437ced08cb2453c37e0d236cf26a
Balaji-G-site <gundlapallibalaji@gmail.com>
2025-05-18 21:25:41 +0530

    hello world started

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=balu
+person2=chinni
+
+echo "$person1:: hii $person2, how are you!"
+echo "$person2:: hi $person1 i'm fine, what about you?"
+echo "$person1:: i'm fine too, what's up"
+echo "$person2:: nothing tell me"
