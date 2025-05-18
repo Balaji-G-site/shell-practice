9a3319d2a8becb625f5b6beb92ed8c5d41031111
Balaji-G-site <gundlapallibalaji@gmail.com>
2025-05-18 21:33:23 +0530

    hello world started

---
+++ 04-args.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1:: hii $person2, how are you!"
+echo "$person2:: hi $person1 i'm fine, what about you?"
+echo "$person1:: i'm fine too, what's up"
+echo "$person2:: nothing tell me"
