61f350551727f5c36cfc2a74dc329271689f54a8
Balaji-G-site <gundlapallibalaji@gmail.com>
2025-05-19 22:19:16 +0530

    hello world started

--- 07-install-command.sh
+++ 07-install-command.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 
-if [USERID -ne 0]
+if [$USERID -ne 0]
 then
 echo "ERROR::please run this script with root access"
 exit 1
@@ -10,3 +10,4 @@ exit 1
 else
 echo "you are running with root access"
 fi
+
