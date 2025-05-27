b60c6c8a3e1dac92824873b752ed5e071cc14c21
Balaji-G-site <gundlapallibalaji@gmail.com>
2025-05-27 15:13:28 +0530

    hello world started

---
+++ 10-command-install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR::Please run this script with root access"
+    exit 1
+else
+    echo "You are running with root access"
+fi
+
+dnf list installed nginx
+
+if [ $? -nq 0 ]
+then
+    echo "Installing nginx...
+    dnf install nginx -y
+    if [ $? -eq 0]
+    then
+        echo "Installing nginx is...SUCCESSFUL"
+    else
+        echo "Installing nginx is...FAILURE"
+        exit 1
+    fi
+else
+    echo "nginx is already installed...nothing to do
+fi
